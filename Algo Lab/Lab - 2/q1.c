6ff2fafcacba9c705ddf4bf912d3fbc8960a1165
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-01-17 12:10:45 +0530

    Update q1.c

--- Algo Lab/Lab - 2/q1.c
+++ Algo Lab/Lab - 2/q1.c
@@ -3,15 +3,16 @@ int smallest(int a,int b){
     return (a>b?a:b);
 }
 int main(){
-    int m, n, t, gcd;
+    int m=0, n=0, t=0, gcd=0, opCount=0;
     printf("Enter two numbers: ");
     scanf("%d %d",&m,&n);
     t = smallest(m,n);
     for(int i=t;i>0;i--){
+        opCount++;
         if(m%i==0 && n%i==0){
             gcd = i;
             break;
         }
     }
-    printf("GCD: %d\n",gcd);
+    printf("GCD of %d and %d is: %d\nOperation count is: %d\n", m, n, gcd, opCount);
 }
