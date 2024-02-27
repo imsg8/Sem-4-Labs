013c0cb3a57cb4b43f137e089709697746cebf62
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-02-27 12:12:01 +0530

    Update table-creation.sql

--- DBS Lab/Extra-Practise/table-creation.sql
+++ DBS Lab/Extra-Practise/table-creation.sql
@@ -21,7 +21,7 @@ insert into customer values(2,'Mohan','Tiger Circle Manipal','Medium');
 insert into customer values(3,'Jacob','Main Road Udupi','Medium');
 
 insert into sales values(101,to_date('27-Jan-2023','DD-MON-YYYY'),1,'Sugar',10,50);
-insert into sales values(102,to_date('20-Dec-2022','DD-MON-YYYY'),1,'Rice',5,40);
+insert into sales values(102,to_date('20-Dec-2022','DD-MON-YYYY'),2,'Rice',5,40);
 insert into sales values(103,to_date('12-Nov-2022','DD-MON-YYYY'),1,'Sunflower Oil',1,150);
-insert into sales values(104,to_date('25-Dec-2022','DD-MON-YYYY'),1,'Rice',2,40);
+insert into sales values(104,to_date('25-Dec-2022','DD-MON-YYYY'),3,'Rice',2,40);
 insert into sales values(105,to_date('21-Nov-2022','DD-MON-YYYY'),1,'Sunflower Oil',2,150);
