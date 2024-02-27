44065b341a608c8c3e16b12345aa9cc7d694a038
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-02-27 12:23:31 +0530

    Update answers.sql

--- DBS Lab/Extra-Practise/answers.sql
+++ DBS Lab/Extra-Practise/answers.sql
@@ -6,3 +6,6 @@ select cname from customer where address like '%Manipal%';
 select sum(qty*unitprice) as TotalAmount from sales where sales.cid = (select cid from customer where cname = 'Mohan');
 
 3)
+select sum(qty*unitprice) as total from sales where cid in(select cid from customer where cname='Mohan');
+select cname from customer where cid in(select cid from sales group by cid having count(distinct(item))=(select count(distinct item) from sales));
+--Thanks Shagnik Guha for answer to Query 3 :)
