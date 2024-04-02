ba5f664274852638302c3cceb63929f3d4259d4f
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-04-02 19:26:07 +0530

    Update q1.c

--- Algo Lab/Lab - 10/q1.c
+++ Algo Lab/Lab - 10/q1.c
@@ -1,44 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define max 100
 
-void Warshall(int** adj,int n)
-{
+void Warshall(int adj[max][max], int n) {
     int i, j, k;
-    for(k=0;k<n;k++)
-    {
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<n;j++)
-            {
+    for (k = 0; k < n; k++) {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
                 adj[i][j] = adj[i][j] | (adj[i][k] & adj[k][j]);
             }
         }
     }
 }
 
-void main()
-{
+int main() {
     int n;
-    printf("Enter No. of Vertices : \t");
-    scanf("%d",&n);
-    int** adj=(int **)calloc(n,sizeof(int *));
-    printf("\nEnter Adjacency Matrix of Directed Graph : \n");
-    for(int i=0;i<n;i++)
-    {
-        adj[i]=(int *)malloc(n*sizeof(int));
-        for(int j=0;j<n;j++)
-        {
-            scanf("%d",&adj[i][j]);
+    printf("Enter No. of Vertices: ");
+    scanf("%d", &n);
+    int adj[max][max];
+    printf("\nEnter Adjacency Matrix of Directed Graph: \n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
         }
     }
-    Warshall(adj,n);
-    printf("\nAdjacency Matrix of Directed Graph : \n");
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            printf("%d ",adj[i][j]);
+    Warshall(adj, n);
+    printf("\nAdjacency Matrix of Directed Graph: \n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", adj[i][j]);
         }
         printf("\n");
     }
+    return 0;
 }
