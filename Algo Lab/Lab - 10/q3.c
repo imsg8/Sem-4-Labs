fe234b3fcbf12bba2f4446d8b2d9942dafa48b32
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-04-02 19:47:26 +0530

    Update q3.c

--- Algo Lab/Lab - 10/q3.c
+++ Algo Lab/Lab - 10/q3.c
@@ -1,91 +1,59 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define MAX_ITEMS 100
 
-int* subset;
-int l=0;
-int cost=0;
-
-int max(int a,int b)
-{
-    return (a>b)?a:b;
+int max(int a, int b) {
+    return (a > b) ? a : b;
 }
 
-void KnapSack(int **KS,int n,int w,int* V,int* W)
-{
-    int i,j,k;
-    for(i=0;i<=n;i++)
-    {
-        for(j=0;j<=w;j++)
-        {
-            KS[i][j]=0;
-        }
-    }
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=w;j++)
-        {
-            if(j-W[i]>=0)
-            {
-                KS[i][j]=max(KS[i-1][j],V[i]+KS[i-1][j-W[i]]);
+void KnapSack(int KS[][MAX_ITEMS], int n, int w, int V[], int W[]) {
+    int i, j;
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= w; j++) {
+            if (j - W[i] >= 0) {
+                KS[i][j] = max(KS[i - 1][j], V[i] + KS[i - 1][j - W[i]]);
+            } else {
+                KS[i][j] = KS[i - 1][j];
             }
-            else
-            {
-                KS[i][j]=KS[i-1][j];
-            }
-        }
-    }
-    j=w;
-    for(int i=n;i>0;i--)
-    {
-        if(KS[i][j]!=KS[i-1][j])
-        {
-            cost+=V[i];
-            subset[l++]=i;
-            j=j-W[i];
         }
     }
 }
 
-void main()
-{
-    int w,n;
-    printf("Enter No. of Items : \t");
-    scanf("%d",&n);
-    printf("Enter Capacity : \t");
-    scanf("%d",&w);
-    int W[n+1],V[n+1];
-    V[0]=W[0]=0;
-    printf("Enter Weights of Items : \n");
-    for(int i=1;i<=n;i++)
-    {
-        scanf("%d",&W[i]);
+int main() {
+    int w, n, i, j, l = 0, cost = 0;
+    printf("Enter No. of Items: ");
+    scanf("%d", &n);
+    printf("Enter Capacity: ");
+    scanf("%d", &w);
+    int W[n + 1], V[n + 1], subset[n + 1];
+    V[0] = W[0] = 0;
+    printf("Enter Weights of Items: \n");
+    for (i = 1; i <= n; i++) {
+        scanf("%d", &W[i]);
     }
-    printf("Enter Prices of Items : \n");
-    for(int i=1;i<=n;i++)
-    {
-        scanf("%d",&V[i]);
+    printf("Enter Prices of Items: \n");
+    for (i = 1; i <= n; i++) {
+        scanf("%d", &V[i]);
     }
-    int **KS=(int**)calloc(n+1,sizeof(int*));
-    for(int i=0;i<=n;i++)
-    {
-        KS[i]=(int*)malloc((w+1)*sizeof(int));
-    }
-    subset=(int*)calloc(n+1,sizeof(int));
-    KnapSack(KS,n,w,V,W);
-    printf("\nKnapSack Table is \n");
-    for(int i=0;i<=n;i++)
-    {
-        for(int j=0;j<=w;j++)
-        {
-            printf("%d\t",KS[i][j]);
+    int KS[n + 1][MAX_ITEMS];
+    KnapSack(KS, n, w, V, W);
+    printf("\nKnapSack Table:\n");
+    for (i = 0; i <= n; i++) {
+        for (j = 0; j <= w; j++) {
+            printf("%d\t", KS[i][j]);
         }
         printf("\n");
     }
-    printf("\nKnapSack Items are : \t");
-    for(int i=l;i>0;i--)
-    {
-        printf("%d ",subset[i]);
+    printf("\nKnapSack Items: ");
+    for (i = n; i > 0; i--) {
+        if (KS[i][w] != KS[i - 1][w]) {
+            cost += V[i];
+            subset[l++] = i;
+            w -= W[i];
+        }
     }
-    printf("\n\nCost is : %d\t",cost);
-    printf("\n\n");
+    for (i = l - 1; i >= 0; i--) {
+        printf("%d ", subset[i]);
+    }
+    printf("\n\nCost: %d\n", cost);
+    return 0;
 }
