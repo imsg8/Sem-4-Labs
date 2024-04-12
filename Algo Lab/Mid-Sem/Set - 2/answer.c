4f6e0a6201fd3aab092e04afef932bfa7536f7d6
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-04-12 12:12:55 +0530

    Update answer.c

--- Algo Lab/Mid-Sem/Set - 2/answer.c
+++ Algo Lab/Mid-Sem/Set - 2/answer.c
@@ -46,3 +46,55 @@ int main() {
 
     return 0;
 }
+
+
+
+// CACHE
+/*
+
+#include<stdio.h>
+int i,n,arr[25];
+int min(int arr[], int n, int start, int end, int ans1){
+	if(end==n){
+		return ans1;
+	}
+	if(arr[start]<ans1){
+		ans1 = arr[start];
+	}
+	int mid = (start+end)/2;
+	min(arr,n,start,mid,ans1);
+	min(arr,n,mid+1,end,ans1);
+}
+
+int max(int arr[], int n, int start, int end, int ans2){
+	if(end==n){
+		return ans2;
+	}
+	if(arr[start]>ans2){
+		ans2 = arr[start];
+	}
+	int mid = (start+end)/2;
+	max(arr,n,start,mid,ans2);
+	max(arr,n,mid+1,end,ans2);
+}
+
+
+int main(){
+	printf("Enter size of array: ");
+	scanf("%d",&n);
+	printf("Enter elements: ");
+	for(i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	int tempa = arr[0];
+	int maximumnumber = max(arr,n,0,n,tempa);
+	int minimumnumber = min(arr,n,0,n,tempa);
+
+	printf("\n \n");
+
+	printf("Minimum is %d\n",minimumnumber);
+	printf("Maximum is %d\n",maximumnumber);
+}
+
+
+*/
